ccced181daffbce88ed6c45a3587490cdc99103d
dominik-pichler <info@dominik-pichler.com>
2024-07-11 10:28:08 +0200

    up

--- mutant_based_testing/test/test_mbt_model.py
+++ mutant_based_testing/test/test_mbt_model.py
@@ -1,7 +1,20 @@
-from mbt_model import is_even
+from mbt_model import add,subtract,multiply,divide
 import pytest
 
-def test_is_even():
-    assert is_even(2)
-    assert not is_even(3)
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, -1) == -2
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(-1, -1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, -1) == 1
+
+def test_divide():
+    assert divide(6, 3) == 2
+    with pytest.raises(ValueError):
+        divide(1, 0)
 
